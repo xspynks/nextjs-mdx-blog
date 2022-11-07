ad31e783be64d04c0a07cbb735fac1be3a251a3c
Rafael de Paula <xspynks@gmail.com>
2022-11-07 01:42:14 -0300

    first commit! :)

---
+++ contentlayer.config.js
@@ -0,0 +1,31 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+const computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+  },
+};
+
+const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "blog/*.md",
+  bodyType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    content: {
+      type: "string",
+      required: true,
+    },
+  },
+  computedFields,
+}));
+
+export default makeSource({
+  contentDirPath: "content",
+  documentTypes: [Post],
+  mdx: {},
+});
